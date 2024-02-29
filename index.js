c92b91539cd20df1d008b433bd5bc4c812b442f3
RossiFabricio <fabrytsd@icloud.com>
2024-02-29 12:18:19 -0300

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,51 @@
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    muñeco.src = "./img/cerrado.jpg";
+  } else {
+    muñeco.src = "./img/robot.png";
+    tituloMensaje.textContent = "No se encontro mensaje";
+    parrafo.textContent = "Ingresa un texto que desees encriptar o desencriptar";
+    swal("Uhh!", "Deberias ingresar un texto", "warning");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "./img/abierto.jpg";
+    } else {
+      muñeco.src = "./img/robot.png";
+      tituloMensaje.textContent = "No se encontro mensaje";
+      parrafo.textContent = "Ingresa un texto que desees encriptar o desencriptar";
+      swal("Uhh!", "Deberias ingresar un texto", "warning");
+    }
+}
